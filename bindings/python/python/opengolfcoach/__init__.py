ea48685e4e9c20475221e31607bf622c51f82954
Chad Rockey <chadrockey@gmail.com>
2026-02-24 06:21:50 -0600

    v0.2.0

--- bindings/python/python/opengolfcoach/__init__.py
+++ bindings/python/python/opengolfcoach/__init__.py
@@ -21,5 +21,4 @@ Example:
 
 from .opengolfcoach_python import calculate_derived_values
 
-__version__ = "0.1.0"
-__all__ = ["calculate_derived_values", "__version__"]
+__all__ = ["calculate_derived_values"]
